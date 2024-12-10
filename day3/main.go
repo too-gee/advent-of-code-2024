79e72a979c4117d75574d81c4f48958b0162b7ec
too-gee <116376+too-gee@users.noreply.github.com>
2024-12-09 23:42:56 -0600

    Add day 3

---
+++ day3/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+)
+
+func main() {
+	// Open the file
+	file, err := os.Open("input.txt")
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	// Read line by line
+	allMatches := [][]string{}
+	re := regexp.MustCompile(`mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\)`)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		matches := re.FindAllStringSubmatch(line, -1)
+
+		allMatches = append(allMatches, matches...)
+	}
+
+	// part 1 && part 2
+
+	runningTotal := 0
+	switchedRunningTotal := 0
+	enabled := true
+
+	for _, match := range allMatches {
+		if match[0] == "do()" {
+			enabled = true
+		} else if match[0] == "don't()" {
+			enabled = false
+		} else {
+			a, _ := strconv.Atoi(match[1])
+			b, _ := strconv.Atoi(match[2])
+
+			runningTotal += a * b
+
+			if enabled {
+				switchedRunningTotal += a * b
+			}
+		}
+	}
+
+	fmt.Printf("Running total: %d\n", runningTotal)
+	fmt.Printf("Switched running total: %d\n", switchedRunningTotal)
+}
