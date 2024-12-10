f1afa5aa75455a37fee519180fbdaaab9fc1e1f9
too-gee <116376+too-gee@users.noreply.github.com>
2024-12-09 23:42:33 -0600

    Add day 1

---
+++ day1/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	// Open the file
+	file, err := os.Open("input.txt")
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	// Read line by line
+	list1 := make([]int, 0)
+	list2 := make([]int, 0)
+
+	for i := 0; scanner.Scan(); i++ {
+		line := scanner.Text()
+
+		parts := strings.Fields(line)
+
+		num1, _ := strconv.Atoi(parts[0])
+		num2, _ := strconv.Atoi(parts[1])
+
+		list1 = append(list1, num1)
+		list2 = append(list2, num2)
+	}
+
+	// part 1
+	differences := 0
+
+	sort.Ints(list1)
+	sort.Ints(list2)
+
+	for i := range list1 {
+		differences += int(math.Abs(float64(list1[i] - list2[i])))
+	}
+
+	fmt.Printf("The total difference is %d\n", differences)
+
+	// part 2
+	similarity := 0
+
+	for _, needle := range list1 {
+		for _, haystack := range list2 {
+			if needle == haystack {
+				similarity += needle
+			}
+		}
+	}
+
+	fmt.Printf("The similarity score is %d\n", similarity)
+}
