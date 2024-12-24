ce166b9f7f585bb60abe47d70468c3742c323860
too-gee <116376+too-gee@users.noreply.github.com>
2024-12-24 13:26:49 -0600

    Switch away from brute force for correct answer

--- day12/main.go
+++ day12/main.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"math"
 	"os"
-	"slices"
 	"strings"
 )
 
@@ -36,11 +35,7 @@ func main() {
 	bulkPrice := 0
 	for _, region := range regions {
 		area, _ := region.grid.measure()
-		sides := region.grid.sideCount()
-
-		for _, void := range region.voids {
-			sides += void.sideCount()
-		}
+		sides := region.grid.dumbSideCount()
 
 		bulkPrice += area * sides
 		fmt.Printf("A region of %s plants with price %d * %d = %d.\n", region.plantType, area, sides, area*sides)
@@ -95,63 +90,6 @@ type Grid [][]string
 type Region struct {
 	plantType string
 	grid      Grid
-	voids     []Grid
-}
-
-func (g Grid) equalTo(other Grid) bool {
-	if g.width() != other.width() || g.height() != other.height() {
-		return false
-	}
-
-	for y := range g {
-		for x := range g[y] {
-			if g[y][x] != other[y][x] {
-				return false
-			}
-		}
-	}
-
-	return true
-}
-
-func (g Grid) blank() bool {
-	for y := range g {
-		for x := range g[y] {
-			if g[y][x] != "." {
-				return false
-			}
-		}
-	}
-
-	return true
-}
-
-func (g Grid) full() bool {
-	for y := range g {
-		for x := range g[y] {
-			if g[y][x] == "." {
-				return false
-			}
-		}
-	}
-
-	return true
-}
-
-func (g Grid) invert() Grid {
-	tmp := makeGrid(g.width(), g.height())
-
-	for y := range g {
-		for x := range g[y] {
-			if g[y][x] == "x" {
-				tmp[y][x] = "."
-			} else {
-				tmp[y][x] = "x"
-			}
-		}
-	}
-
-	return tmp
 }
 
 func (g Grid) draw() {
@@ -159,20 +97,43 @@ func (g Grid) draw() {
 }
 
 func (g Grid) drawWithMarkers(markers map[Coord]string) {
+	yMin, yMax := g.height()-1, 0
+	xMin, xMax := g.width()-1, 0
+
+	for y := range g {
+		for x := range g[y] {
+			if g[y][x] != "." {
+				yMin = int(math.Min(float64(yMin), float64(y)))
+				yMax = int(math.Max(float64(yMax), float64(y)))
+				xMin = int(math.Min(float64(xMin), float64(x)))
+				xMax = int(math.Max(float64(xMax), float64(x)))
+			}
+		}
+	}
+
 	output := ""
 
-	for y := -2; y < g.height()+2; y++ {
-		for x := -2; x < g.width()+2; x++ {
+	borderThickness := 1
+	bufferThickness := 1
+
+	for y := yMin - borderThickness - bufferThickness; y <= yMax+borderThickness+bufferThickness; y++ {
+		for x := xMin - borderThickness - bufferThickness; x <= xMax+borderThickness+bufferThickness; x++ {
 			loc := Coord{x, y}
 
 			// print the border
-			if x == -2 || x == g.width()+1 || y == -2 || y == g.height()+1 {
+			if x >= xMin-borderThickness-bufferThickness && x < xMin-bufferThickness ||
+				(y >= yMin-borderThickness-bufferThickness && y < yMin-bufferThickness) ||
+				(x > xMax+bufferThickness && x <= xMax+borderThickness+bufferThickness) ||
+				(y > yMax+bufferThickness && y <= yMax+borderThickness+bufferThickness) {
 				output += "██"
 				continue
 			}
 
 			// print a buffer
-			if x == -1 || x == g.width() || y == -1 || y == g.height() {
+			if (x >= xMin-borderThickness && x < xMin) ||
+				(y >= yMin-borderThickness && y < yMin) ||
+				(x > xMax && x <= xMax+bufferThickness) ||
+				(y > yMax && y <= yMax+bufferThickness) {
 				output += "　"
 				continue
 			}
@@ -231,7 +192,6 @@ func (g Grid) drawWithMarkers(markers map[Coord]string) {
 	}
 
 	fmt.Print(output)
-
 }
 
 func (g Grid) width() int {
@@ -240,16 +200,6 @@ func (g Grid) width() int {
 
 func (g Grid) height() int {
 	return len(g)
-}
-
-func (g Grid) copy() Grid {
-	tmp := makeGrid(g.width(), g.height())
-
-	for y := range g {
-		copy(tmp[y], g[y])
-	}
-
-	return tmp
 }
 
 func (g Grid) measure() (int, int) {
@@ -301,7 +251,7 @@ func (g Grid) regions() []Region {
 		plantTypeCoord := flood.firstCell("x")
 		plantType := g[plantTypeCoord.y][plantTypeCoord.x]
 
-		regions = append(regions, Region{plantType: plantType, grid: flood, voids: flood.voids()})
+		regions = append(regions, Region{plantType: plantType, grid: flood})
 	}
 
 	return regions
@@ -401,25 +351,7 @@ func (g Grid) flood(loc Coord) Grid {
 	return regionGrid
 }
 
-func (g *Grid) forward(loc *Coord) {
-	if oldLoc := (*g).firstCell("@"); g.isInGrid(oldLoc) {
-		(*g)[oldLoc.y][oldLoc.x] = "x"
-	}
-
-	(*g)[(*loc).y][(*loc).x] = "@"
-
-	(*loc).y--
-}
-
-func (g *Grid) rotate(loc *Coord, dir string) {
-	// mark the pivot point
-	prevValue := ""
-
-	if (*loc).x > -1 && (*loc).y > -1 {
-		prevValue = (*g)[(*loc).y][(*loc).x]
-		(*g)[(*loc).y][(*loc).x] = "!"
-	}
-
+func (g *Grid) rotate(dir string) {
 	// rotate the grid
 	width, height := (*g).width(), (*g).height()
 	result := makeGrid(width, height)
@@ -436,177 +368,53 @@ func (g *Grid) rotate(loc *Coord, dir string) {
 		}
 	}
 
-	// find the new pivot point and unmark it
-	if (*loc).x > -1 && (*loc).y > -1 {
-		*loc = result.firstCell("!")
-
-		result[(*loc).y][(*loc).x] = prevValue
-	}
-
 	*g = result
 }
 
-func (g Grid) currentView(loc Coord) Grid {
-	window := makeGrid(3, 3)
+func (g Grid) dumbSideCount() int {
+	runs := g.dumbRunCount()
+	g.rotate("R")
+	runs += g.dumbRunCount()
 
-	for y := loc.y - 1; y <= loc.y+1; y++ {
-		for x := loc.x - 1; x <= loc.x+1; x++ {
+	return runs
+}
+
+func (g Grid) dumbRunCount() int {
+	runs := 0
+
+	for y := 0; y < g.height()+1; y++ {
+		// collapse each row into unique sections
+		row := []string{}
+
+		for x := 0; x < g.width(); x++ {
+			upper := ""
+			lower := ""
+
+			if g.isInGrid(Coord{x: x, y: y - 1}) {
+				upper = g[y-1][x]
+			} else {
+				upper = "."
+			}
+
 			if g.isInGrid(Coord{x: x, y: y}) {
-				window[y-loc.y+1][x-loc.x+1] = g[y][x]
+				lower = g[y][x]
+			} else {
+				lower = "."
+			}
+
+			currentCells := upper + lower
+			if len(row) == 0 || row[len(row)-1] != currentCells {
+				row = append(row, currentCells)
+			}
+		}
+
+		// count the number of "edges"
+		for _, section := range row {
+			if section[0] != section[1] {
+				runs++
 			}
 		}
 	}
 
-	return window
-}
-
-func (g Grid) sideCount() int {
-	// track our progress around the perimeter
-	moves := []string{}
-
-	// starting conditions
-	grid := g.copy()
-	moveId := ""
-	direction := Direction("N")
-	loc := g.firstCell("x")
-	prevLoc := loc
-
-	// go until we get a repeat
-	for {
-		view := grid.currentView(loc)
-		move := view.nextMove()
-
-		moveId = fmt.Sprintf("loc: %d,%d  dir: %s  move: %s  view: %v", loc.x, loc.y, direction, move, view)
-
-		// we got a repeat, stop here
-		if slices.Contains(moves, moveId) {
-			break
-		}
-
-		moves = append(moves, moveId)
-
-		if move == "F" {
-			grid.forward(&loc)
-		}
-
-		if move == "R" {
-			grid.rotate(&loc, "R")
-			direction.rotate("R")
-		}
-
-		if move == "L" {
-			grid.rotate(&loc, "L")
-			direction.rotate("L")
-		}
-
-		if prevLoc != loc {
-			prevLoc = loc
-		}
-	}
-
-	// start from the first occurrence of the repeated move and count the turns
-	first := slices.Index(moves, moveId)
-	totalTurns := 0
-
-	for i := first; i < len(moves); i++ {
-		if strings.Contains(moves[i], "R") || strings.Contains(moves[i], "L") {
-			totalTurns++
-		}
-	}
-
-	return totalTurns
-}
-
-func (g Grid) nextMove() string {
-	if g[0][0] == "." && g[0][1] == "." && g[0][2] == "." {
-		if g[1][0] == "." {
-			return "L"
-		} else {
-			return "R"
-		}
-	}
-
-	if g[0][0] == "." && g[0][1] == "." && g[1][0] == "." {
-		return "L"
-	}
-
-	if g[1][0] == "@" && g[0][1] == "x" {
-		return "F"
-	}
-
-	if g[1][0] != "." && g[2][0] == "." && g[2][1] == "@" {
-		return "R"
-	}
-
-	return "F"
-}
-
-type Direction string
-
-func (d *Direction) rotate(dir string) {
-	dirs := []string{"N", "E", "S", "W"}
-
-	i := 0
-
-	if dir == "L" {
-		i = 1
-	} else {
-		i = -1
-	}
-
-	currentIndex := slices.Index(dirs, string(*d))
-	newIndex := int(math.Abs(float64((currentIndex + i) % 4)))
-
-	*d = Direction(dirs[newIndex])
-}
-
-func (g Grid) voids() []Grid {
-	voidCells1 := g.voidMiddles()
-	g.rotate(&Coord{-1, -1}, "R")
-	voidCells2 := g.voidMiddles()
-	voidCells2.rotate(&Coord{-1, -1}, "L")
-
-	voidCells := makeGrid(g.width(), g.height())
-
-	for y := range voidCells {
-		for x := range voidCells[y] {
-			if voidCells1[y][x] == "x" && voidCells2[y][x] == "x" {
-				voidCells[y][x] = "x"
-			}
-		}
-	}
-
-	tmp := voidCells.floods()
-	grids := []Grid{}
-
-	for _, flood := range tmp {
-		if !flood.equalTo(voidCells.invert()) && !flood.full() && !flood.blank() {
-			grids = append(grids, flood)
-		}
-	}
-
-	return grids
-}
-
-func (g Grid) voidMiddles() Grid {
-	voidCells := makeGrid(g.width(), g.height())
-
-	for y, row := range g {
-		first := slices.Index(row, "x")
-		slices.Reverse(row)
-		last := len(row) - slices.Index(row, "x")
-		slices.Reverse(row)
-
-		if first == -1 || last == -1 {
-			continue
-		}
-
-		for x := first + 1; x < last; x++ {
-			if g[y][x] == "." {
-				voidCells[y][x] = "x"
-			}
-		}
-	}
-
-	return voidCells
+	return runs
 }
