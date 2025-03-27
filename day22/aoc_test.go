a733706ea352b6651dfecbf723e761c730464fae
too-gee <116376+too-gee@users.noreply.github.com>
2025-03-26 22:21:55 -0500

    Day 23 part 1 solution

--- day22/aoc_test.go
+++ day22/aoc_test.go
@@ -6,19 +6,20 @@ import (
 
 type testCase struct {
 	fileName string
-	function func([]int, int) int
+	function func([]int) int
 	iters    int
 	expSum   int
 }
 
 func TestAll(t *testing.T) {
 	cases := []testCase{
-		{"input_small.txt", Solve, 2000, 37327623},
+		{"input_small.txt", Part1, 2000, 37327623},
+		{"input.txt", Part1, 2000, 20506453102},
 	}
 
 	for _, c := range cases {
 		input := readInput(c.fileName)
-		sum := c.function(input, c.iters)
+		sum := c.function(input)
 		if sum != c.expSum {
 			t.Errorf("%s: expected sum: %d, got sum: %d", c.fileName, c.expSum, sum)
 		}
