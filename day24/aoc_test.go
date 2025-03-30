8376c16e5510d1fa1210fdc64f345bf660465085
too-gee <116376+too-gee@users.noreply.github.com>
2025-03-30 00:52:06 -0500

    Add new test case

--- day24/aoc_test.go
+++ day24/aoc_test.go
@@ -6,14 +6,16 @@ import (
 
 type testCase struct {
 	fileName string
-	function func(Connections) int
-	exp      int
+	function func(Connections) string
+	exp      string
 }
 
 func TestAll(t *testing.T) {
 	cases := []testCase{
-		{"input_small.txt", Part1, 4},
-		{"input_medium.txt", Part1, 2024},
+		{"input_small.txt", Part1, "4"},
+		{"input_medium.txt", Part1, "2024"},
+		{"input.txt", Part1, "56729630917616"},
+		{"input_small2.txt", Part2, "z00,z01,z02,z05"},
 	}
 
 	for _, c := range cases {
@@ -21,7 +23,7 @@ func TestAll(t *testing.T) {
 		output := c.function(conns)
 
 		if output != c.exp {
-			t.Errorf("%s: expected: %d, got: %d", c.fileName, c.exp, output)
+			t.Errorf("%s: expected: %s, got: %s", c.fileName, c.exp, output)
 		}
 	}
 }
